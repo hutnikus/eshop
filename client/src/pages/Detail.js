f968e64578bbca89b5b1c0b58e8849c9288911df
Norbert Moravko <nmoravko@fides.cz>
2023-01-09 18:51:22 +0100

    epic win bro

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,9 +1,20 @@
+import * as React from 'react';
+import { useLocation } from "react-router-dom"
+import { useEffect } from "react";
+
+
+
 const Detail = () => {
-  return (
-    <div>
-      <h1>Detail</h1>
-    </div>
-  )
+
+    let location = useLocation();
+    console.log(location, " useLocation Hook");
+    const data = location.state?.data;
+
+    return (
+        <div>
+            <h1>Detail {data.name}</h1>
+        </div>
+    )
 }
 
 export default Detail
