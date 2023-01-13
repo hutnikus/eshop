1991675f8bf1ee7edf501ce96bc1d77e2f627417
Norbert Moravko <nmoravko@fides.cz>
2023-01-13 14:46:45 +0100

    prve zmeny navbar

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -2,13 +2,14 @@ import { Link } from "react-router-dom"
 
 const Navigation = () => {
     return (
-        <div>
-            <nav>
-                <li><Link to='/'>Home</Link></li>
+        <nav className="nav">
+            <li className="home"><Link to='/'>Home</Link></li>
+            <ul>
                 <li><Link to='/Catalog'>Products</Link></li>
-                <li><Link to='About'>About</Link></li>
-            </nav>
-        </div>
+            </ul>
+
+            <li><Link to='About'>About</Link></li>
+        </nav>
     )
 }
 
