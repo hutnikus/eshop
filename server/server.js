6154da42ba05362c4921c2a87a88e9a8c8e8a85f
hutnikus <hutnand@gmail.com>
2023-01-18 18:36:01 +0100

    added route list to root

--- server/server.js
+++ server/server.js
@@ -6,12 +6,19 @@ const app = express()
 const apiRouter = require("./routes/api")
 
 app.get('/', (req, res) => {
-    res.send('Pee Pee Poo Poo')
+    res.send(`
+    routes <br>
+    <br>
+    /api <br>
+    /api/products <br>
+    /api/product/:id <br>
+    <br>
+    /static/:name <br>
+    `)
 })
 
 app.use('/api',apiRouter)
 
 app.use('/static/',express.static('data/static'))
-
 
 app.listen(3000, console.log("Server running on http://localhost:3000/"))
