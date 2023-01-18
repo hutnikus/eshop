949a7e08e9ad5414e415bbf2fe8241aed6602109
Norbert Moravko <nmoravko@fides.cz>
2023-01-18 17:46:40 +0100

    dalsie linky

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -6,6 +6,8 @@ const Navigation = () => {
             <li className="home"><Link to='/'>Home</Link></li>
             <li><Link to='/Catalog'>Products</Link></li>
             <li><Link to='About'>About</Link></li>
+            <li><Link to='/Ucet'>Ucet</Link></li>
+            <li><Link to='/Kosik'>Kosik</Link></li>
         </div>
     )
 }
---
+++ client/src/pages/Ucet.js
@@ -0,0 +1,9 @@
+const Ucet = () => {
+  return (
+    <div>
+      <h1>Ucet</h1>
+    </div>
+  )
+}
+
+export default Ucet
