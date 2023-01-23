b41fb5b8cf73bda5bb61c1435f8c2a840c2dd26f
Norbert Moravko <nmoravko@fides.cz>
2023-01-23 17:26:38 +0100

    to co som zabudol

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,28 +1,44 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import { Button, CardActionArea, CardActions } from '@mui/material';
 import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
-const Product = ({ product, onClickHander }) => {
 
-  const [isInCart, setIsInCart] = useState(false)
+
+export default function Product({ product, onClickHander }) {
+
 
   return (
-    <div style={{
-      width: "150px",
-      border: '1px solid black',
-      display: 'flex',
-      flexDirection: 'column',
-      margin: '5px'
-    }}>
-      <h2><Link to={`/Catalog/${product.id}`} state={{ data: product }}>{product.name}</Link></h2>
-      <div>{product.price}czk</div>
-      <div><Link to={`/Catalog/${product.id}`}><img src={product.img} alt='' height={150} width={150} /></Link></div>
-      <button onClick={() => {
-        setIsInCart(true)
-        onClickHander(product)
-      }}>Buy</button>
-      <div>{isInCart && "In cart"}</div>
-    </div>
-  )
+    <Card sx={{ maxWidth: 345 }}>
+      <CardActionArea>
+      <Link to={`/Catalog/${product.id}`}>
+      <CardMedia
+          component="img"
+          height="160"
+          image={product.img}
+          alt="green iguana"
+        />
+        </Link>
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            <Link style={{ textDecoration: 'none', color: '#1976d2' }} to={`/Catalog/${product.id}`} state={{ data: product }}>{product.name}</Link>
+            <div style={{ fontSize: '15px' }}>{product.price}czk</div>
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Lizards are a widespread group of squamate reptiles, with over 6,000
+            species, ranging across all continents except Antarctica
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Share
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
-
-export default Product
---
+++ client/src/components/productclone.js
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+
+const Product = ({ product, onClickHander }) => {
+
+  const [isInCart, setIsInCart] = useState(false)
+
+  return (
+    <div style={{
+      width: "150px",
+      border: '1px solid black',
+      display: 'flex',
+      flexDirection: 'column',
+      margin: '5px'
+    }}>
+      <h2><Link style={{ textDecoration: 'none', color: '#1976d2' }} to={`/Catalog/${product.id}`} state={{ data: product }}>{product.name}</Link></h2>
+      <div>{product.price}czk</div>
+      <div><Link to={`/Catalog/${product.id}`}><img src={product.img} alt='' height={150} width={150} /></Link></div>
+      <button onClick={() => {
+        setIsInCart(true)
+        onClickHander(product)
+      }}>Buy</button>
+      <div>{isInCart && "In cart"}</div>
+    </div>
+  )
+}
+
+export default Product
